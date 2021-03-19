7bfe7aa2ae790decf8376d23695074bafaee4cf8
Kingsley Atuba <atubak40@gmail.com>
2021-03-20 00:48:19 +0100

    project complete

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,37 @@
 <template>
 	<div>
 		<h1>Atuba Reaction Timer</h1>
-		<button @click="start">play</button>
+		<button @click="start" :disabled="isPlaying">play</button>
+		<Block v-if="isPlaying" :delay="delay" @end="endGame" />
+		<Results v-if="showResults" :score="score" />
 	</div>
 </template>
 
 <script>
+import Block from './components/Block.vue'
+import Results from './components/Results'
 export default {
 	name: 'App',
-	components: {},
+	components: { Block, Results },
 	data() {
 		return {
 			isPlaying: false,
 			delay: null,
+			score: null,
+			showResults: false,
 		}
 	},
 	methods: {
 		start() {
 			this.delay = 2000 + Math.random() * 5000
 			this.isPlaying = true
-			console.log(this.delay)
+			// console.log(this.delay)
+			this.showResults = false
+		},
+		endGame(reactionTime) {
+			this.score = reactionTime
+			this.isPlaying = false
+			this.showResults = true
 		},
 	},
 }
@@ -33,5 +45,21 @@ export default {
 	text-align: center;
 	color: #444;
 	margin-top: 60px;
+}
+button {
+	background: #0faf87;
+	color: white;
+	border: none;
+	padding: 8px 16px;
+	border-radius: 4px;
+	font-size: 16px;
+	letter-spacing: 1px;
+	cursor: pointer;
+	margin: 10px;
+}
+
+button[disabled] {
+	opacity: 0.2;
+	cursor: not-allowed;
 }
 </style>
